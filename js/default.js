017e325c5dfc16cd90704f2c30d66ea27a040e75
atsushi461 <g1627030@st.t-kougei.ac.jp>
2017-06-19 17:50:27 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,14 +1,22 @@
 var number = Math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('数あてゲーム。0~2の数字を入力してください！'));
-
+var i ;
+var a ;
 var message;
+
+while(a<1){
 if(answer === number){
   message = 'あたり！';
+  a=0;
 }else if (answer < number){
   message = '残念！もっと大きいです';
+  a=1;
 }else if (number < answer){
   message = '残念！もっと小さいです';
+  a=1;
 }else{
   message = '0~3の数字を入力してください';
+  a=1;
+}
 }
 document.getElementById('choice').textContent = message;
